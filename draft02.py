0b5251c365f5675ea4d27ac924e8b56c38e4e4b1
viccos <victor.costa@e3dc.com>
2023-10-24 14:51:35 +0200

    generating pwl waveform for the two mosfets

--- draft02.py
+++ draft02.py
@@ -32,7 +32,12 @@ ar_t_pwl = np.arange(start = 0,stop = T, step = 1/fsamp_pwl)
 ar_sin = np.sin(w0*ar_t_pwl + phi)
 
 # define time_lims
-tsum = dead_time/4
+
+
+
+# ===================================================================
+phi = 0*d2r
+tsum = dead_time/4 + phi*(T/(2*np.pi))
 li_t_lims = []
 li_test = []
 for n in range(nsteps+1):
@@ -44,7 +49,7 @@ for n in range(nsteps+1):
 amp = 0
 idx_tlim = 0
 tlim = li_t_lims[idx_tlim]
-li_pwl_amp = []
+li_pwl_amp1 = []
 for t in ar_t_pwl:
     if(t>tlim):
         idx_tlim += 1
@@ -54,11 +59,43 @@ for t in ar_t_pwl:
             amp = np.sin(w0*(tlim - T_step/2) + phi)
         else:
             amp = 0
-    li_pwl_amp.append(amp)
+    li_pwl_amp1.append(amp)
 
 
-plt.plot(ar_t_pwl, ar_sin)
-plt.plot(ar_t_pwl, li_pwl_amp)
-plt.plot(li_t_lims, li_test)
+
+# ===================================================================
+phi = 180*d2r
+tsum = dead_time/4 + phi*(T/(2*np.pi))
+li_t_lims = []
+li_test = []
+for n in range(nsteps+1):
+    li_t_lims.append(tsum)
+    tsum += T_step
+    li_test.append(1)
+
+
+amp = 0
+idx_tlim = 0
+tlim = li_t_lims[idx_tlim]
+li_pwl_amp2 = []
+for t in ar_t_pwl:
+    if(t>tlim):
+        idx_tlim += 1
+        if(idx_tlim<len(li_t_lims)):
+            # idx_tlim = min(idx_tlim,len(li_t_lims)-1)
+            tlim = li_t_lims[idx_tlim]
+            amp = np.sin(w0*(tlim - T_step/2) + phi)
+        else:
+            amp = 0
+    li_pwl_amp2.append(amp)
+
+
+
+
+plt.plot(ar_t_pwl, ar_sin, label = "sin", color = "r")
+plt.plot(ar_t_pwl, li_pwl_amp1, label = "pwl_1", color = "g")
+plt.plot(ar_t_pwl, li_pwl_amp2, label = "pwl_2", color = "b")
+plt.plot(li_t_lims, li_test, label = "lims", color = "k")
+plt.legend()
 plt.grid()
 
